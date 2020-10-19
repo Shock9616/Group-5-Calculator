632e070c5fae4d966f14eb495a7b61902066c476
Kaleb Rosborough <kaleb@rosborough.ca>
2020-10-19 10:21:01 -0700

    Making calculator.c conform to CS50 style standards

---
+++ README.md
@@ -0,0 +1,3 @@
+# Group-5-Calculator
+
+This calculator accepts up to 5 numbers and performs multiple mathematical operations on the given numbers. (Following order of operations)
---
+++ calculator.c
@@ -0,0 +1,14 @@
+/*
+A calculator that accepts up to 5 numbers and performs
+multiple mathematical operations on the given numbers.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+
+
+    return 0;
+}
